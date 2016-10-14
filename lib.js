08ce273e93910c8dc74e23ac6a5c457ff7f6c611
Brad Lindgren <bplindgren91@gmail.com>
2016-10-14 09:34:43 -0500

    shopping cart functional complete

--- lib.js
+++ lib.js
@@ -12,21 +12,83 @@ const cart =
     items
   })
 
-const listedPrice =
-  listing =>
-    name =>
-      name === listing.name
-        ? listing.price
-        : 0
+// const listedPrice =
+//   listing =>
+//     name =>
+//       name === listing.name
+//         ? listing.price
+//         : 0
+
+// const listedPrice =
+//   function (listing) {
+//     return function (name) {
+//       if (name === listing.name) {
+//         return listing.price
+//       } else {
+//         return 0
+//       }
+//     }
+//   }
 
 /**
  * transform carts into an array of { customer, total }
  */
-const calculateTotals =
-  listings =>
-    carts => {
-      // TODO
-    }
+ // Peter's Functional Solution
+ // const calculateTotals2 =
+ //   (listings) =>
+ //     (carts) => {
+ //       return carts.map(
+ //         cart => ({
+ //           customer: cart.customer,
+ //           total:
+ //            cart.items.reduce(
+ //             (prior, current) =>
+ //              prior +
+ //                listings
+ //                  .filter(l => l.name === current)
+ //                 .map(l => l.price)
+ //                 .reduce((prior, current) => prior + current, 0)
+ //                ,
+ //             0
+ //           )
+ //         })
+ //       )
+ //     }
+
+ // My Functional solution
+ const calculateTotals =
+   listings =>
+     carts =>
+      carts.map(cart => ({
+         customer: cart.customer,
+         total: cart.items.reduce((prior, current) => prior + listings.filter(listing => listing.name === current).map(listing => listing.price).reduce((prior, current) => prior + current, 0), 0)
+       })
+     )
+
+// Imperative solution
+// const calculateTotals =
+//   function (listings) {
+//     return function (carts) {
+//       const result = []
+//
+//       for (let cart of carts) {
+//         const customerTotal = { customer: cart.customer }
+//         let total = 0
+//
+//         for (let item of cart.items) {
+//           for (let listing of listings) {
+//             if (listing.name === item) {
+//               total += listing.price
+//             }
+//           }
+//         }
+//
+//         customerTotal.total = total
+//         result.push(customerTotal)
+//       }
+//       return result
+//     }
+//   }
 
 module.exports = {
   listing,
